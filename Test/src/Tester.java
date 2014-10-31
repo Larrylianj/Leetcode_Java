2237129a3e272547e9e3d24992989a1adb9dc6de
SiyangLiang <liangsiyang@guest-wireless-nup-nat-206-117-89-004.usc.edu>
2014-10-31 08:46:26 -0700

    4th Commit

---
+++ Test/src/Tester.java
@@ -0,0 +1,14 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "//home//../r/";
+		String[] all = s.split("/");
+		for(int i = 0;i < all.length;i++){
+			System.out.println(all[i]);
+		}
+		
+	}
+
+}
